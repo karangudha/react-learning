aea631a2e4fa76fbaa7f8c6a0b326e717535890b
Karan <karankgudha@gmail.com>
2025-11-14 19:17:06 +0530

    first commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='root'>
+      <h1 id = 'child1'>Hello, React!</h1>
+      <h2 id = 'child2'>Welcome to your React application.</h2>
+    </div>
+  )
+}
+
+export default App
